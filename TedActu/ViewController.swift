2aefb979211cd1c1c4e7579ed0252bed53e4a665
IsaSam <isaacsamuel27@gmail.com>
2020-03-19 17:57:54 -0400

    Project created

---
+++ TedActu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TedActu
+//
+//  Created by Isaac Samuel on 3/19/20.
+//  Copyright © 2020 Isaac Samuel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
