374dc7dba9ed1775e7319382d11fcfbb32d6ff45
IsaSam <isaacsamuel27@gmail.com>
2020-04-02 16:49:16 -0400

    cleaning codes

--- TedActu/PlaylistViewController.swift
+++ TedActu/PlaylistViewController.swift
@@ -12,29 +12,15 @@ import Alamofire
 
 class PlaylistViewController: UIViewController {
     
-    ////new
     let UPLOADS_PLAYLIST_ID =  "UUYwjO810TxGsyEG9ytlRtbQ"
     let API_KEY = "AIzaSyBQqgKRuhh2JuqOpKpwRULqtydyYSRkvn4"
     let urlString = "https://www.googleapis.com/youtube/v3/playlistItems"
     
-    var items: [[String: Any]] = []
     var snippet: [[String: Any]] = []
         var snippet1: [[String: Any]] = []
         var resourceId1: [[String: Any]] = []
-    var videoTitle: [[String: Any]] = []
-    var videoDesc: [[String: Any]] = []
-    var videoId: [[String: Any]] = []
-    var videoTitleString: [String] = []
-    var videoDescString: [String] = []
-    var videoIdString: [String] = []
-//    var videoTitleString: String?
-//    var videoDescString: String?
-//    var videoIdString: String?
     
     var posts: [[String: Any]] = []
-//    var  videos = [video]()
-//
-    ///
 
        @IBOutlet weak var tableView: UITableView!
         var videos:[video] = [video]()
@@ -43,17 +29,9 @@ class PlaylistViewController: UIViewController {
         override func viewDidLoad() {
             super.viewDidLoad()
             
-   ////         let model = VideoModel()
-      //      self.videos = model.getVideo()
-            
-   ////         model.getFeedVideos()
-        
-     //       self.videos = model.getVideos()
-            
             tableView.delegate = self
             tableView.dataSource = self
             
-   ///         getFeedVideos()
             getFeedsVideos()
             
 
@@ -109,84 +87,6 @@ class PlaylistViewController: UIViewController {
                         
             }
     }
-    
-    
-    func getFeedVideos(){
-            
-            // Alamofire 4
-            let parameters: Parameters = ["part": "snippet", "playlistId": UPLOADS_PLAYLIST_ID, "key": API_KEY]
-            
-            
-            Alamofire.request(urlString, method: .get, parameters: parameters, encoding: URLEncoding.queryString)
-                .downloadProgress(queue: DispatchQueue.global(qos: .utility)) { progress in
- ///                   print("Progress: \(progress.fractionCompleted)")
-                }
-                .validate { request, response, data in
-                    // Custom evaluation closure now includes data (allows you to parse data to dig out error messages if necessary)
-                    return .success
-                }
-                .responseJSON { response in
-                    debugPrint(response)
-                    
-                                var  videos = [video]()
-                                let video1 = video()
-                    
-                                if let JSON = response.result.value as? [String:Any]  {//as? [String:Any] {
-                                    
-                                    for items in JSON["items"] as! NSArray {
-                                        
-                                   
-                                     //   self.items = items as! [[String : Any]]
-                                        self.items.append(items as! [String : Any])
-                                        for snippet in self.items{
-                                            
-                                            print("begin--")
-                                            self.snippet.append(snippet)
-                                       //     print(snippet)
-                                            print("*--")
-                                            }
-                                        for items2 in self.snippet{
-                                            let snippetDic = (items2 as AnyObject).value(forKey: "snippet")
-                                            let titleDic = (snippetDic as AnyObject).value(forKey: "title")
-                                            let descDic = (snippetDic as AnyObject).value(forKey: "description")
-                                            let resourceId = (snippetDic as AnyObject).value(forKey: "resourceId")
-                                            let videoId = (resourceId as AnyObject).value(forKey: "videoId")
-//                                       //     self.videoTitleString.append(titleDic as! String)
-//                                            self.videoTitleString = (titleDic as! String)
-//                                       //     self.videoDescString.append(descDic as! String)
-//                                            self.videoDescString = (descDic as! String)
-//                                       //     self.videoIdString.append(videoId as! String)
-//                                            self.videoIdString = (videoId as! String)
-                                            
-                                            video1.videoTitle = (titleDic as! String)
-                                            video1.videoDes = (descDic as! String)
-                                            video1.videoId = (videoId as! String)
-                                            
-                                            videos.append(video1)
-                                      //      self.tableView.reloadData()
-                                            
-                                            print("***")
-                                            
-                                            let titleDicString = titleDic as? [[String: Any]]
-                                            let descDicString = descDic as? [[String: Any]]
-                                            let videoDicString = videoId as? [[String: Any]]
-                                            
-                                            if titleDicString != nil || descDicString != nil || videoDicString != nil{
-                                                self.videoTitle = titleDicString!
-                                                self.videoDesc = descDicString!
-                                                self.videoId = videoDicString!
-                                            }
-
-                                        }
-                                            
-                                        }
-                                    
-   ///                              print("json \(JSON)")
-                               }
-                            
-                }
-        }
-    
         
     }//End viewController
 
@@ -194,13 +94,9 @@ class PlaylistViewController: UIViewController {
 
     extension PlaylistViewController : UITableViewDataSource , UITableViewDelegate{
         
-        
         func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-         ///   return videos.count
-           /// return posts.count
             return snippet1.count
         }
-        
         
         func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
             
@@ -218,14 +114,7 @@ class PlaylistViewController: UIViewController {
             video1.videoId =  (resource["videoId"] as? String)!
             self.videos.append(video1)
     
-//            let videoTitle = videos[indexPath.row].videoTitle
-//            let videoTitle = videoTitleString[indexPath.row]
-//            let label = cell.viewWithTag(2) as! UILabel
-//            label.text = videoTitle
-            
-            
             // Construct the video thumbnail url
-//            let videoThumbnailUrlString = "https://i1.ytimg.com/vi/" + videos[indexPath.row].videoId + "/maxresdefault.jpg"
             let videoThumbnailUrlString = "https://i1.ytimg.com/vi/" + videos[indexPath.row].videoId + "/maxresdefault.jpg"
             
             
