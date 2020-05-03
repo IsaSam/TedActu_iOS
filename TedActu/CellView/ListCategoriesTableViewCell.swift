574738d86975353a04bbfc48dcdf454089069a96
IsaSam <isaacsamuel27@gmail.com>
2020-05-03 03:39:39 -0400

    ListTable Categories created

---
+++ TedActu/CellView/ListCategoriesTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  ListCategoriesTableViewCell.swift
+//  TedActu
+//
+//  Created by Isaac Samuel on 5/3/20.
+//  Copyright © 2020 Isaac Samuel. All rights reserved.
+//
+
+import UIKit
+
+class ListCategoriesTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageListCategories: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TedActu/Controllers/ListCategoriesViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ListCategoriesViewController.swift
+//  TedActu
+//
+//  Created by Isaac Samuel on 5/3/20.
+//  Copyright © 2020 Isaac Samuel. All rights reserved.
+//
+
+import UIKit
+
+class ListCategoriesViewController: UIViewController {
+    
+    var myImages: [UIImage] = [
+        UIImage(named: "Politique_BG_Blanc.jpg")!,
+        UIImage(named: "Savoir+_BG_Blanc.jpg")!,
+        UIImage(named: "Societe_BG_Blanc.jpg")!,
+        UIImage(named: "Sport_BG_Blanc.jpg")!,
+        UIImage(named: "Culture_BG_Blanc.jpg")!,
+        UIImage(named: "Anket_BG_Blanc.jpg")!
+        
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
+extension ListCategoriesViewController : UITableViewDataSource , UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myImages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCategoriesCell", for: indexPath) as! ListCategoriesTableViewCell
+    
+        cell.imageListCategories?.image = myImages[indexPath.row]
+        
+        return cell
+        
+    }
+ 
+    
+    
+}
